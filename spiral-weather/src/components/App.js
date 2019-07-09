70f1c4ee0e53a38254b7dd90b7131625a9add705
r <kubr2017@gmail.com>
2019-07-09 10:45:44 -0400

    Feat: Add <MainBox>

--- spiral-weather/src/components/App.js
+++ spiral-weather/src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import MainBox from './MainBox';
 
 const App = () => {
-  return <div>App</div>;
+  return <div>App
+           <MainBox/>
+         </div>;
 };
-    
+
 export default App;
