2f390ae43390b3695bddc89c5891570652293df4
r <kubr2017@gmail.com>
2019-07-09 10:49:27 -0400

    Feat: Add <CitiesBox>

--- spiral-weather/src/components/App.js
+++ spiral-weather/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import MainBox from './MainBox';
+import CitiesBox from'./CitiesBox';
 
 const App = () => {
   return <div>App
            <MainBox/>
+           <CitiesBox/>
          </div>;
 };
 
---
+++ spiral-weather/src/components/CitiesBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CitiesBox = () => {
+  return <div>CitiesBox</div>;
+};
+
+export default CitiesBox;
