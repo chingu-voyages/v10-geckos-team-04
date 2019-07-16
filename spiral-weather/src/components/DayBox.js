86f99199634765d3712b19604f48f918a92e3707
r <kubr2017@gmail.com>
2019-07-16 14:04:43 -0400

    Feat: Wire Up response data to component (console.log result)

--- spiral-weather/src/components/DayBox.js
+++ spiral-weather/src/components/DayBox.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-const DayBox = () => {
-  return <div className = 'DayBox-wrap'>DayBox</div>;
+class DayBox extends Component {
+
+  renderList(){
+    if (this.props.posts.data){
+      let data = this.props.posts.data;
+      let hourlyData = data.hourly.data;
+      console.log('Inside Daybox, data:',data)
+      console.log('Inside Daybox, hourlyData:',hourlyData)
+      return(
+        <div>Here suppose data</div>
+      )
+    };
+
+  };
+  render(){
+    return(
+      <div className = 'DayBox-wrap'>
+        DayBox
+        {this.renderList()}
+      </div>)
+  }
 };
 
-export default DayBox;
+const mapStateToProps = state =>{
+  return {posts:state.posts}
+}
+
+export default connect(mapStateToProps)(DayBox);
